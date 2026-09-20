007404b8f436de2d9b7ecdf3ce9fb64e6c63609a
Narges Salari <narges.sa1359@gmail.com>
2026-09-20 11:16:37 -0400

    update the code

---
+++ server/routes/gifts.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import path from 'path'
+import {fileURLToPath} from 'url'
+import giftData from '../data/gift.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const router = express.Router()
+router.get('/',(req,res) =>{
+    res.status(200).json(giftData)
+})
+
