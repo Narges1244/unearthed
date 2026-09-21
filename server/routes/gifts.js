0e898cca3b3b98a80718d8694ae4b8479a56b9ea
Narges Salari <narges.salari@expressgene.us>
2026-09-20 21:29:02 -0400

    update the app by improving the list of object should showed

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -11,3 +11,10 @@ router.get('/',(req,res) =>{
     res.status(200).json(giftData)
 })
 
+router.get('/:giftId',(req,res) =>{
+    res.status(200).sendFile(path.resolve(__dirname,'..public/gift.html'))
+})
+
+export default router
+
+
